d94d36865b85a70044d76291a5c9d8b3e605ea1f
Giotje_ <devgio@icloud.com>
2021-11-16 18:22:38 +0100

    added collatz conjecture

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-import math
-input_number = int(input("Choose a number: "))
+from time import perf_counter_ns
+it_count = 0
+n = 0
 
 
-if input_number % 2 == 0:
-    new_number = int(math.floor(input_number / 2))
-    print(new_number)
-else:
-    new_number = int(math.floor(input_number * 3 + 1))
-    input_number * 3 + 1
-    print(new_number)
+def Select_Number():
+    print("")
+    sn = int(input("Selecteer het nummer dat je wilt gebruiken:"))
+    if (sn == 0):
+        print("Dit is geen correct nummer")
+        print()
+        print()
+        Select_Number()
+    else:
+        Calculate(sn)
+
+
+def Calculate(n):
+    global it_count
+    it_count = 0
+
+    start = perf_counter_ns()
+
+    while n != 1:
+        if (n % 2):
+            n = (n*3+1)
+            print(n)
+            it_count += 1
+        else:
+            n = (n//2)
+            print(n)
+            it_count += 1
+
+    end = perf_counter_ns()
+    print("Het getal heeft" + str(n) + " gehaald met maar: " + str(it_count) +
+          " sprongen (Tijd dat dit heeft gekost: " + format(end-start) + " nanoseconden.)")
+    print()
+    Select_Number()
+
+
+Select_Number()
