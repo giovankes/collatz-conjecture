d9b38e67f088a764b67c821ce5e3a4a84a575466
Giotje_ <devgio@icloud.com>
2021-11-16 10:48:28 +0100

    lmao

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 input_number = int(input("Choose a number"))
 
+while input_number > 0:
+    if (input_number % 2) == 0:
+        input_number / 2
+        print(input_number)
+    else:
+        input_number * 3 + 1 
+        print(input_number)
+
 
