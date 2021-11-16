d99e1f6792721bd7580896c6ab58f08ffbc53e94
Giotje_ <devgio@icloud.com>
2021-11-16 15:33:31 +0100

    format

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ while input_number > 0:
         input_number / 2
         print(input_number)
     else:
-        input_number * 3 + 1 
+        input_number * 3 + 1
         print(input_number)
-
-
